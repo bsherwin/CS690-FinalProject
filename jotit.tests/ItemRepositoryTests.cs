7446319abbc91147550506ec0ac0273982561926
Brian Sherwin <brian@surewinsolutions.com>
2026-04-18 20:12:26 -0400

    Add test class

---
+++ jotit.tests/ItemRepositoryTests.cs
@@ -0,0 +1,12 @@
+using JotIt.Data;
+using JotIt.Models;
+
+namespace JotIt.Tests;
+
+public class ItemRepositoryTests
+{
+    [Fact]
+    public void Testproject() {
+        Assert.True(true);
+    }
+}
